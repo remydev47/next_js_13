61222aa4a05c3970bbf7a326b69162588615f0e0
remydev47 <brianrimidev47@gmail.com>
2023-05-09 16:45:13 +0300

    first commit

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,22 @@
+//[...nextauth] means Dynamic next authentication the folder is named that since its the current best practice
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google-privider"
+import { signIn } from "next-auth/react";
+
+//declare the Handler
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: '',
+            clientSecret: '',
+        })
+    ],
+    async session({ session }) {
+
+    },
+    async signIn({ profile }) {
+
+    }
+})
+export { handler as GET, handler as POST }
---
+++ components/Provider.jsx
@@ -0,0 +1,16 @@
+'use clent';
+import { SessionProvider } from "next-auth/react"
+
+const Provider = ({children, session }) => {
+
+
+  return (
+   <SessionProvider
+    session={session}
+   >
+     {children}
+   </SessionProvider>
+  )
+}
+
+export default Provider
