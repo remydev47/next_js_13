e73b504ca74a264f444de6f020620e0cdd4d0c56
remydev47 <brianrimidev47@gmail.com>
2023-06-22 15:47:29 +0300

    debu

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,56 +1,48 @@
 //[...nextauth] means Dynamic next authentication the folder is named that since its the current best practice
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google"
-import { signIn } from "next-auth/react";
-import User from "@models/user";
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
-import { connectToDB } from "@utils/database";
-
-//declare the Handler
+import User from '@models/user';
+import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        })
-    ],
-    callbacks: {
-        async session({ session }) {
-            const sessionUser = await User.findOne({
-                email: session.user.email
-            })
-    
-            session.user.id = sessionUser._id.toString();
-    
-            return session;
-        },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    })
+  ],
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
 
-        async signIn({ profile }) {
-            try {
-                await connectToDB();
-    
-                //check if the user exists
-                const useExist = await User.findOne({
-                    email: profile.email
-                });
-    
-                //if the user doesnt exist, create it
-                if(!useExist) {
-                    await User.create({
-                        email: profile.email,
-                        name: profile.name.replace(" ", "").toLowerCase(),
-                        image: profile.picture
-                    });
-                }
-    
-                return true;
-            } catch (error) {
-                console.log(error);
-                return false;
-            }
+      return session;
+    },
+    async signIn({ account, profile, user, credentials }) {
+      try {
+        await connectToDB();
+
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
+
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
         }
-    }
-    //updating the session
+
+        return true
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false
+      }
+    },
+  }
 })
+
 export { handler as GET, handler as POST }
--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
+let isConnected = false;//track connection established
 
 export const connectToDB = async () => {
     mongoose.set('strictQuery', true);
@@ -13,7 +13,7 @@ export const connectToDB = async () => {
         await mongoose.connect(process.env.MONGODB_URI, {
             dbName: "share_prompts",
             useNewUrlParser: true,
-            useUnifiedUrlTopology: true,
+            useUnifiedTopology: true,
         })
 
         isConnected =  true;
