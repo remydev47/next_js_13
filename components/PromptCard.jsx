03ad49bffc362171659b7e1cb3a9fcd61edc8d38
remydev47 <brianrimidev47@gmail.com>
2023-06-28 20:40:00 +0300

    prompt card

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -13,24 +13,24 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     setTimeout(() => setCopied(""), 3000 )
   };
   return (
-    <div className="promt_card">
+    <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-          <Image 
+          {/* <Image 
             src={post.creator.image} 
             alt="user_image"
             width={40}
             height={40}
             className="rounded-full object-contain"
-          />
+          /> */}
 
           <div className="flex flex-col">
-            <h3 className="font-satoshi font-semibold text-gray-900">
+            {/* <h3 className="font-satoshi font-semibold text-gray-900">
               {post.creator.username}
             </h3>
             <p className="font-inter text-sm text-gray-500">
               {post.creator.email}
-            </p>
+            </p> */}
           </div>
 
         </div>
